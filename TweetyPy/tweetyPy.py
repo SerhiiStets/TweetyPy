363cd19c4e37cf5679be0be27fa60592dc0525f3
SerhiiStets <stets.serhii@gmail.com>
2023-02-05 22:28:22 +0200

    structure change

--- TweetyPy/tweetyPy.py
+++ TweetyPy/tweetyPy.py
@@ -38,46 +38,77 @@ class TweetyPy:
 
     def __init__(self, API_key: str, API_secret: str, AI_token: str, AI_secret: str) -> None:
         """Initialize TweetyPy object and check for authorization."""
-        self.twitter_auth = tweepy.OAuthHandler(API_key, API_secret)
-        self.twitter_auth.set_access_token(AI_token, AI_secret)
-        self.twitter_auth.secure = True
-        self.twitter_api = tweepy.API(self.twitter_auth)
-        self._check_authentication_to_twitter()
+        self.logger = self._create_logger()
+        self.chosen_topic = ""
+        self.tweets = []
+        self.API_key = API_key
+        self.API_secret = API_secret
+        self.AI_token = AI_token
+        self.AI_secret = AI_secret
+        self.twitter_api: tweepy.API = self._twitter_authorization()
 
-    def _check_authentication_to_twitter(self) -> None:
-        """Trying to get user @twitter from id to see if auth worked. Raise error if not."""
+    def _create_logger(self) -> logging.Logger:
+        logger = logging.getLogger('TweetyPy')
+        logger.setLevel(level=logging.INFO)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        logger.addHandler(console_handler)
+        return logger
+
+    def _twitter_authorization(self) -> tweepy.API:
+        """
+        Trying to auth to twitter and get user @twitter from id to see if auth worked.
+        Raise error if not.
+        """
         try:
-            self.twitter_api.get_user(user_id=783214)
+            twitter_auth = tweepy.OAuthHandler(self.API_key, self.API_secret)
+            twitter_auth.set_access_token(self.AI_token, self.AI_secret)
+            twitter_auth.secure = True
+            twitter_api = tweepy.API(twitter_auth)
+            twitter_api.get_user(user_id=783214)  # Checking auth with real Twitter request
+            self.logger.info("Successfully Authenticated to Twitter API!")
+            return twitter_api
         except tweepy.errors.Unauthorized:
+            self.logger.critical("Incorrect API keys, authentication error.")
             raise tweepy.TweepyException("Incorrect API keys, authentication error.")
 
-    def get_trending_topics_by_country(self, country_id: CountryID) -> list[dict]:
+    def _get_trending_topics_by_country(self, country_id: CountryID) -> list[dict]:
         """Returns a list of top trending topics in twitter for given country."""
         return self.twitter_api.get_place_trends(id=country_id.value)[0]['trends']
 
-    def get_tweets_by_topic(self, topic_name: str) -> list[str]:
+    def _get_tweets_by_topic(self, topic_name: str) -> list[str]:
         """Return an array of tweets by given topic with removed url links in them."""
         tweets_iterator = tweepy.Cursor(self.twitter_api.search_tweets, q=f"{topic_name} -filter:retweets",
                                         result_type="mixed", lang="en").items(2500)
         return [re.sub(r'https\S+', '', tweet.text) for tweet in tweets_iterator]
 
-    @staticmethod
-    def generate_tweet(tweets: list[str]) -> str:
-        """Retuens newly create tweet from received list of tweets."""
-        if not tweets:
+    def generate_tweet(self, country_id: CountryID) -> str:
+        """Generates tweet from random topic for given country."""
+        trending_topics_list = self._get_trending_topics_by_country(country_id)
+        self.chosen_topic = trending_topics_list[randint(0, 14)]['name']
+        self.logger.info(f"The chosen topic is {self.chosen_topic}")
+        self.tweets = self._get_tweets_by_topic(self.chosen_topic)
+        self.logger.info(f"{len(self.tweets)} tweets were read")
+        if not self.tweets:
             raise IndexError("Tweets list is empty, no tweets were given.")
-        text_model = markov_generate(tweets)
-        return text_model.make_short_sentence(280)
+        text_model = markov_generate(self.tweets)
+        new_tweet = text_model.make_short_sentence(280)
+        self.logger.info(f"Generated tweet - {new_tweet}")
+        return new_tweet
 
-    @staticmethod
-    def generate_wordcloud(tweets: str) -> Image.Image:
+    def generate_wordcloud(self) -> Image.Image:
         """Generating wordcloud with given topic and tweets."""
+        if not self.tweets:
+            raise IndexError("No tweets were parsed from Twitter.")
+        str_tweets = ' '.join(self.tweets)
         current_directory = path.dirname(__file__)
         # Reads the mask image of twitter logo to numpy array
         twitter_mask = array(Image.open(path.join(current_directory, "twitter_logo.png")))
         new_word_cloud = WordCloud(background_color="white", mask=twitter_mask, contour_width=5,
                                    contour_color='steelblue')
-        new_word_cloud.generate(tweets)
+        new_word_cloud.generate(str_tweets)
+        logging.info("Generated wordcloud image.")
         return new_word_cloud.to_image()
 
     def send_tweet(self, tweet: str, image: Image.Image = None) -> None:
@@ -96,41 +127,26 @@ class TweetyPy:
             self.twitter_api.update_status_with_media(tweet, filename="TweetyPy WordClouod", file=fp)
         else:
             self.twitter_api.update_status(tweet)
+        self.logger.info("Tweet is now live!")
 
 
 def run_tweetyPy():
     """Starting point for server and local runs of TweetyPy."""
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout, format="%(asctime)s - %(levelname)s - %(message)s")
-    try:
-        API_key = getenv("API_key", "optional-default")
-        API_secret = getenv("API_secret", "optional-default")
-        AI_token = getenv("AT_token", "optional-default")
-        AI_secret = getenv("AT_secret", "optional-default")
+    API_key = getenv("API_key", "optional-default")
+    API_secret = getenv("API_secret", "optional-default")
+    AI_token = getenv("AT_token", "optional-default")
+    AI_secret = getenv("AT_secret", "optional-default")
 
-        tweetyPy = TweetyPy(API_key, API_secret, AI_token, AI_secret)
-        logging.info("Successfully Authenticated to Twitter API!")
+    tweetyPy = TweetyPy(API_key, API_secret, AI_token, AI_secret)
 
-        trending_topics_list = tweetyPy.get_trending_topics_by_country(CountryID.USA)
+    generated_tweet = tweetyPy.generate_tweet(CountryID.USA)
 
-        random_topic = trending_topics_list[randint(0, 14)]['name']
-        logging.info(f"The chosen topic is {random_topic}")
+    if randint(0, 3) == 3:
+        # For roughly every 4th tweet we want to generate wordcloud for it
+        wordcloud_image = tweetyPy.generate_wordcloud()
+        tweetyPy.send_tweet(f"Most popular words\n {tweetyPy.chosen_topic}", wordcloud_image)
 
-        tweets = tweetyPy.get_tweets_by_topic(random_topic)
-        logging.info(f"{len(tweets)} tweets were read")
-
-        generated_tweet = tweetyPy.generate_tweet(tweets)
-        logging.info(f"Generated tweet - {generated_tweet}")
-
-        if randint(0, 3) == 3:
-            # For roughly every 4th tweet we want to generate wordcloud for it
-            wordcloud_image = tweetyPy.generate_wordcloud(' '.join(tweets))
-            tweetyPy.send_tweet(f"Most popular words\n {random_topic}", wordcloud_image)
-            logging.info("Generated wordcloud image.")
-
-        tweetyPy.send_tweet(generated_tweet)
-        logging.info("Tweet is now live!")
-    except Exception as e:
-        logging.critical(e)
+    tweetyPy.send_tweet(generated_tweet)
 
 
 if __name__ == "__main__":
